87027a75d3a0f6033cb3bf174a1f69f8afcda0ae
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2024-12-06 18:08:04 -0800

    Activate debug console in QEMU and test code in stage 2

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ $(DISK_IMG): bootloader
 	dd if=$(STAGE2_BIN) of=$(DISK_IMG) bs=512 seek=1 conv=notrunc
 
 run:
-	$(QEMU) -fda $(DISK_IMG)
+	$(QEMU) -debugcon stdio -fda $(DISK_IMG)
 
 clean:
 	$(MAKE) -C $(BOOTLOADER_DIR) clean
--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -7,12 +7,28 @@ org 0x0000                ; stage 2 is loaded at 0x1000:0000
                           ; where we loaded stage 2 so we need to make sure
                           ; ds (data segment) points to the same segment
   mov ds, ax
+
+  mov si, debug_message
+  call .write_debug
+
   mov si, message
   call .print_string
 
 .loop_forever:
   jmp .loop_forever
 
+.write_debug:
+  .next_char_debug:
+    lodsb
+    cmp al, 0
+    je .done_debug
+    mov dx, 0xE9
+    out dx, al
+    jmp .next_char_debug
+  .done_debug:
+    ret
+
 %include "utils.asm"
 
 message db 'Stage 2 loaded!', 0
+debug_message db 'Testing QEMU Debug', 0
