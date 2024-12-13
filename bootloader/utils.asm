f73943b0f2d184f8aeb34a47ebd6f9bf0ed1a42b
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2024-12-12 16:11:24 -0800

    push and pop registers before printing and more documentation

--- bootloader/utils.asm
+++ bootloader/utils.asm
@@ -4,6 +4,7 @@ bits 16
 ; Inputs:
 ;     - si: string to print (has to be 0-terminated)
 .print_string:
+  pusha                 ; Push all general purpose registers to stack (ax, cx, dx, bx, sp, bp, si, di)
   .next_char:
     lodsb
     cmp al, 0           ; Reached the end of the string once we hit a zero
@@ -18,4 +19,6 @@ bits 16
     mov al, 0x0A        ; Print line feed (\n)
     mov ah, 0x0E
     int 0x10
+
+    popa                ; Pop all general purpose registers from the stack
     ret
