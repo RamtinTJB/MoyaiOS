768eeee4db41886c9b1c52fd0af16d370ca4a138
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2025-01-05 18:20:56 -0800

    Programmable Interval Timer initial commit

--- kernel/pit.cpp
+++ kernel/pit.cpp
@@ -2,27 +2,92 @@
 #include "hw_interface.hpp"
 #include <debug.hpp>
 
-void pit_irq() {
-    asm volatile(
-        "add $12, %%esp\n\t"
+static uint32_t tick_count = 0;
+
+extern "C" void pit_irq_handler();
+
+extern "C" __attribute__((naked)) void pit_irq() {
+    // asm volatile(
+    //     "cli\n\t"
+    //     "pushal\n\t"
+    //     :
+    //     :
+    //     : "memory"
+    // );
+
+    // debug_printf("PIT IRQ\n");
+    // tick_count++;
+    // interrupt_done(0);
+
+    // asm volatile("sti");
+    // asm volatile(
+    //     "popal\n\t"
+    //     "iretl\n\t"
+    //     :
+    //     :
+    //     : "memory"
+    // );
+    __asm__ volatile(
+        "cli\n\t"
         "pushal\n\t"
-        :
-        :
-        : "memory"
-    );
-
-    debug_printf("PIT IRQ\n");
-    interrupt_done(0);
-
-    asm volatile(
+        "call pit_irq_handler\n\t"
         "popal\n\t"
-        "iretl\n\t"
-        :
-        :
-        : "memory"
+        "iret\n\t"
     );
+}
+
+extern "C" void pit_irq_handler() {
+  debug_printf("PIT_IRQ\n");
+  tick_count++;
+  interrupt_done(0);
+}
+
+void pit_send_command(uint8_t cmd) {
+  outb(PIT_REG_COMMAND, cmd);
+}
+
+void pit_send_data(uint16_t data, uint8_t counter) {
+  uint8_t port;
+  if (counter == PIT_OCW_COUNTER_0) port = PIT_REG_COUNTER0;
+  else if (counter == PIT_OCW_COUNTER_1) port = PIT_REG_COUNTER1;
+  else if (counter == PIT_OCW_COUNTER_2) port = PIT_REG_COUNTER2;
+  else return;
+
+  outb(port, (uint8_t)data);
+}
+
+uint8_t pit_read_data(uint16_t counter) {
+  uint8_t port;
+  if (counter == PIT_OCW_COUNTER_0) port = PIT_REG_COUNTER0;
+  else if (counter == PIT_OCW_COUNTER_1) port = PIT_REG_COUNTER1;
+  else if (counter == PIT_OCW_COUNTER_2) port = PIT_REG_COUNTER2;
+  else return 0;
+
+  return inb(port);
+}
+
+void pit_start_counter(uint32_t frequency, uint8_t counter, uint8_t mode) {
+  if (frequency == 0) return;
+
+  uint16_t div = uint16_t(1193181 / (uint16_t)frequency);
+
+  uint8_t ocw = 0;
+  ocw = (ocw & ~PIT_OCW_MASK_MODE) | mode;
+  ocw = (ocw & ~PIT_OCW_MASK_RL) | PIT_OCW_RL_DATA;
+  ocw = (ocw & ~PIT_OCW_MASK_COUNTER) | counter;
+  pit_send_command(ocw);
+
+  pit_send_data(div & 0xff, 0);
+  pit_send_data((div >> 8) & 0xff, 0);
+
+  tick_count = 0;
+}
+
+uint32_t pit_get_tick_count() {
+  return tick_count;
 }
 
 void pit_init() {
     set_int_vect(32, (I86_IRQ_HANDLER)pit_irq);
+    pit_start_counter(100, PIT_OCW_COUNTER_0, PIT_OCW_MODE_SQUAREWAVE);
 }
--- kernel/pit.hpp
+++ kernel/pit.hpp
@@ -1,6 +1,51 @@
 #ifndef MOYAIOS_PIT_HPP_
 #define MOYAIOS_PIT_HPP_
 
+#include <stdint.h>
+
+// ====================== PIT I/O Ports ======================
+#define PIT_REG_COUNTER0            0x40
+#define PIT_REG_COUNTER1            0x41
+#define PIT_REG_COUNTER2            0x42
+#define PIT_REG_COMMAND             0x43
+
+// ====================== Operational Command Bit masks ======================
+#define PIT_OCW_MASK_BINCOUNT       1
+#define PIT_OCW_MASK_MODE           0xE
+#define PIT_OCW_MASK_RL             0x30
+#define PIT_OCW_MASK_COUNTER        0xC0
+
+// ====================== Control Bits ======================
+
+// Binary count mode
+#define PIT_OCW_BINCOUNT_BINARY     0
+#define PIT_OCW_BINCOUNT_BCD        1
+
+// Counter mode
+#define PIT_OCW_MODE_TERMINALCOUNT  0
+#define PIT_OCW_MODE_ONESHOT        0x2
+#define PIT_OCW_MODE_RATEGEN        0x4
+#define PIT_OCW_MODE_SQUAREWAVE     0x6
+#define PIT_OCW_MODE_SOFTWARETRIG   0x8
+#define PIT_OCW_MODE_HARDWARETRIG   0xA
+
+// Data transfer
+#define PIT_OCW_RL_LATCH            0
+#define PIT_OCW_RL_LSBONLY          0x10
+#define PIT_OCW_RL_MSBONLY          0x20
+#define PIT_OCW_RL_DATA             0x30
+
+// The counter we are working with
+#define PIT_OCW_COUNTER_0           0
+#define PIT_OCW_COUNTER_1           0x40
+#define PIT_OCW_COUNTER_2           0x80
+
+void pit_send_command(uint8_t cmd);
+void pit_send_data(uint16_t data, uint8_t counter);
+uint8_t pit_read_data(uint16_t counter);
+uint32_t pit_get_tick_count();
+void pit_start_counter(uint32_t frequency, uint8_t counter, uint8_t mode);
+
 void pit_init();
 
 #endif
